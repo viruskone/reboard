796169bc9cfaa12d022a98d971df89d2f1c174a8
viruskone <adrian.kaczmarek.2010@gmail.com>
2019-11-02 20:52:55 +0100

    domain

---
+++ domain/Auth/AuthenticationRequest.cs
@@ -0,0 +1,8 @@
+namespace Reboard.Domain.Auth
+{
+    public class AuthenticationRequest
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ domain/Reports/Report.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Reboard.Domain.Reports
+{
+    public class Report
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int Downloads { get; set; }
+        public TimeSpan AverageDuration { get; set; }
+        public double Rating { get; set; }
+
+    }
+
+}
